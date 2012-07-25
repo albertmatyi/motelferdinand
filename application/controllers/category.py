8e716a61a46ae2def48067af86e8f160ad1890e5
Matyas Albert Nagy <albertmatyi@gmail.com>
2012-07-25 21:26:29 +0300

    implemented form & table generation /w introspection

--- application/controllers/category.py
+++ application/controllers/category.py
@@ -4,21 +4,41 @@ from flask.templating import render_template
 from application.models import * 
 from flaskext import wtf
 from flask.globals import request
-from flask.helpers import flash
+from flask.helpers import flash, url_for
+from flaskext.wtf.form import Form
+from werkzeug.utils import redirect
 
 CategoryForm = model_form(CategoryModel, wtf.Form)
 
-@app.route("/admin/categories/", methods=["GET"])
-@app.route("/", methods=["GET"])
-def admin_categories():
+@app.route("/admin/categories/", methods=["GET", "POST"])
+def admin_list():
     categories = CategoryModel.all()
     form = CategoryForm(request.form)
-    if form.validate_on_submit():
-        db_obj = CategoryModel()
-        form.populate_obj(db_obj)
-        db_obj.put()
-        flash("Category saved")
-        pass
-    return render_template('/category/admin_list.html', data=categories)
+    if request.method == "POST":
+        if form.validate_on_submit():
+            db_obj = CategoryModel()
+            form.populate_obj(db_obj)
+            db_obj.put()
+            flash("Data saved successfully!", "success")
+            return redirect(url_for('admin_list'))
+            pass
+        else:
+            flash("Invalid data. Check description near fields.", "error")
+            return render_template('/base/admin_edit.html', form=form)
+    form = Form()
+    return render_template('/base/admin_list.html', list=categories, properties=CategoryModel.properties())
     pass
 
+
+@app.route("/admin/categories/new", methods=["GET"])
+def admin_new():
+    form = CategoryForm(request.form)
+    return render_template('/base/admin_edit.html', form=form)
+    pass
+
+@app.route("/admin/categories/<int:mdl_id>", methods=["GET", "POST"])
+def admin_edit(mdl_id):
+    db_obj = CategoryModel.get_by_id(mdl_id);
+    form = CategoryForm(request.form, db_obj)
+    return render_template('/base/admin_edit.html', form=form)
+    pass
