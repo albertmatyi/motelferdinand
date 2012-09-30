1d5b73bb299926a015bd81142f6522574aae0566
Matyas Albert Nagy <albertmatyi@gmail.com>
2012-09-30 22:19:10 +0300

    added hide effect

--- application/static/js/header_fixer.js
+++ application/static/js/header_fixer.js
@@ -13,5 +13,14 @@ var HeaderFixer = {
 			HeaderFixer.$palceholder = $('#header .nav-placeholder');
 			HeaderFixer.headerPos = HeaderFixer.$header.offset().top;
 			$(window).scroll(HeaderFixer.scrollListener);
-		}		
+			HeaderFixer.$header.click(HeaderFixer.headerClickHandler);
+		},
+		'headerClickHandler':function (event){
+			if(event.target == event.currentTarget){
+				HeaderFixer.$header.hide('slide', function(){
+					HeaderFixer.$header.show('slide');
+				});
+				
+			}
+		}
 }
