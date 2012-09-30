f9192f7aa3613d85806f83d79148dbb9c8c4cee2
Matyas Albert Nagy <albertmatyi@gmail.com>
2012-10-01 01:20:35 +0300

    added showhide icon to navbar

--- application/static/js/header_fixer.js
+++ application/static/js/header_fixer.js
@@ -1,26 +1,27 @@
 var HeaderFixer = {
-		'scrollListener': function(event){
-			if($(window).scrollTop() > HeaderFixer.headerPos){
-				HeaderFixer.$header.addClass('navbar-fixed-top');
-				HeaderFixer.$palceholder.show();
-			}else{
-				HeaderFixer.$header.removeClass('navbar-fixed-top');
-				HeaderFixer.$palceholder.hide();
-			}
-		},
-		'activate': function(){
-			HeaderFixer.$header = $('#header .navbar');
-			HeaderFixer.$palceholder = $('#header .nav-placeholder');
-			HeaderFixer.headerPos = HeaderFixer.$header.offset().top;
-			$(window).scroll(HeaderFixer.scrollListener);
-			HeaderFixer.$header.click(HeaderFixer.headerClickHandler);
-		},
-		'headerClickHandler':function (event){
-			if(event.target == event.currentTarget){
-				HeaderFixer.$header.hide('slide', function(){
-					HeaderFixer.$header.show('slide');
-				});
-				
+	'scrollListener' : function(event) {
+		var atTop = $(window).scrollTop() <= HeaderFixer.headerPos;
+		if (!atTop && !HeaderFixer.$header.hasClass('navbar-fixed-top')) {
+			HeaderFixer.$header.addClass('navbar-fixed-top');
+			HeaderFixer.$palceholder.show();
+		} else if (atTop && HeaderFixer.$header.hasClass('navbar-fixed-top')) {
+			HeaderFixer.$header.removeClass('navbar-fixed-top');
+			HeaderFixer.$palceholder.hide();
+			if (!HeaderFixer.$headerContent.is(':visible')) {
+				HeaderFixer.$headerContent.show('slide');
 			}
 		}
+	},
+	'activate' : function() {
+		HeaderFixer.$header = $('#header .navbar');
+		HeaderFixer.$palceholder = $('#header .nav-placeholder');
+		HeaderFixer.headerPos = 5;//HeaderFixer.$header.offset().top;
+		HeaderFixer.$headerContent = $('.navbar-inner', HeaderFixer.$header);
+		$(window).scroll(HeaderFixer.scrollListener);
+		$('.toggle-navbar', HeaderFixer.$header).click(
+				HeaderFixer.headerClickHandler);
+	},
+	'headerClickHandler' : function(event) {
+		HeaderFixer.$headerContent.toggle('slide');
+	}
 }
