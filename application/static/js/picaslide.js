7b0119700781e4c3ab3e250d56a42fa13c9c3eb4
Matyas Albert Nagy <albertmatyi@gmail.com>
2012-10-08 00:07:28 +0300

    fixed image quality problems /w google

--- application/static/js/picaslide.js
+++ application/static/js/picaslide.js
@@ -12,7 +12,7 @@
 	        var picasaAlbum = "<div class='picasa-album slides_container' style=\"height: "+height+"; width: "+width+";\">\n";
 	        $.each(images, function(i, element) {
 	          picasaAlbum += "  <div class='picasa-image' style=\"width: "+width+"; height: "+height+";"+
-	          " background-image: url("+element.url+"); background-size: cover; background-position: center;\""+
+	          " background-image: url("+element.url.replace(/(\/)([^\/]+)$/, '$1s'+Math.max(parseInt(height), parseInt(width))+'/$2')+"); background-size: cover; background-position: center;\""+
 	          ">\n";
 //	          picasaAlbum += "    <a class='picasa-image-large' href='" + element.url + "'>\n";
 //	          picasaAlbum += "      <img class='picasa-image-thumb' src='" + element.thumbs[3].url + "'/>\n";
