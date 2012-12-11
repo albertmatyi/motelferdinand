88898e331f46c9d2bd208a5429486b770cbfd4e5
Matyas Albert Nagy <albertmatyi@gmail.com>
2012-12-11 23:12:45 +0200

    model refactoring

--- application/models/__init__.py
+++ application/models/__init__.py
@@ -3,49 +3,26 @@ Created on Jul 24, 2012
 
 @author: matyas
 '''
-from google.appengine.ext import db
-
-class AbstractModel(db.Model):
-    created = db.DateTimeProperty(auto_now_add=True)
-    
-    def to_dict(self):
-        '''
-            Converts a Model obj, or a model obj array to a dictionary (array of dic
-            tionaries
-        '''
-        arr = [('id',self.key().id())]
-        
-        for key in self.properties():
-            val = getattr(self, key)
-            if val is not None and isinstance(getattr(self.__class__, key), db.ReferenceProperty):
-                val = val.key().id()
-            arr += [(key, unicode(val))]
-        if hasattr(self.__class__, 'dependencies'):
-            for dep in self.__class__.dependencies:
-                val = [el.to_dict() for el in getattr(self, dep)]
-                arr += [(dep, val)]
-        return dict(arr) 
-        pass
-    
-    def delete(self):
-        '''
-            Overrides the default delete behaviour and cascades on all categories
-            and images first
-            @return super.delete()
-        '''
-        if hasattr(self.__class__, 'dependencies'):
-            for dep in self.__class__.dependencies:
-                for obj in getattr(self, dep):
-                    obj.delete()
-        return super(AbstractModel, self).delete()
-        pass
-    pass
-
+# base stuff
+from base import *
 from i18n import *
+
+# categories
 from category import *
+
+# external media references for adding to contents
 from media import *
+
+# [abstract] content
 from content import *
+
+# bookable (dependent on content)
 from bookable import *
+
+# user
 from user import *
+
+# dependent on bookable & user
 from booking import *
+
 from populator import *
