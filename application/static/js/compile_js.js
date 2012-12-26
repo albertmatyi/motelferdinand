9599d1b113a0898ce760b93f37d61a91af7eedda
Matyas Albert Nagy <albertmatyi@gmail.com>
2012-12-26 18:21:22 +0200

    added optimization & minified js. reducing page load by 50%

--- application/static/js/compile_js.js
+++ application/static/js/compile_js.js
@@ -1,3 +1,4 @@
+// used by the compile_js.sh from the apps root
 ({
     appDir: "../",
     baseUrl: "js",
--- compile_js.sh
+++ compile_js.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 cd ./application/static/js
+rm ./main.min.js
 nodejs /usr/local/lib/node_modules/requirejs/bin/r.js -o ./compile_js.js
-mv ../js-build/main.js ./main.min.js
-#rm ../js-build -R
+mv ../js-build/js/main.js ./main.min.js
+rm ../js-build -R
