ca450701cb2ab95985691726f3ee8867c3d2a504
Matyas Albert Nagy <albertmatyi@gmail.com>
2013-03-12 21:07:25 +0100

    renamed shellscripts added oauth2 support for gae deploy

--- commit_deploy.sh
+++ commit_deploy.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-git add .; git commit -a; git push origin master; appcfg.py update .
+./compile_css.sh
+./compile_js.sh
+git add .
+git commit -a
+git push origin master
+appcfg.py --oauth2 update .
