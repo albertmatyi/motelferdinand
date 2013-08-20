22792b9021562088a07d2a676caf99dca76594ba
Matyas Albert Nagy <albertmatyi@gmail.com>
2013-08-20 09:06:55 +0200

    lazy load galleries

--- application/static/js/helpers/picaslide.js
+++ application/static/js/helpers/picaslide.js
@@ -1,4 +1,5 @@
 /*global define */
+/*global window */
 /*global _ */
 /*global $ */
 /*global jQuery */
@@ -7,10 +8,13 @@ define([
 	'lib/jquery',
 	'lib/picasa',
 	'lib/slides',
-	'elements/fullscreen'
+	'elements/fullscreen',
+	'elements/progress'
 ],
-function (jq, picasa, slides, fullscreen) {
+function (jq, picasa, slides, fullscreen, progressHelper) {
 	'use strict';
+
+	var GALLERIES = [];
 
 	var	fullscreenUrlPreprocessor = function (url, width) {
 		return getUrlForSize(url, width, 'w');
@@ -34,39 +38,77 @@ function (jq, picasa, slides, fullscreen) {
 
 	(function ($) {
 		$.fn.picaslide = function (slideOpts, successCallback) {
-			var scope = $(this);
-			var user = scope.attr('data-picaslide-username');
-			var album = scope.attr('data-picaslide-albumid');
-			if (!user || user.length < 3 || !album || album.length < 1) {
-				return;
-			}
-			var w = scope.width();
-			var width = w + 'px';
-			var height = w * 3 / 4 + 'px';
-			scope.css('width', width);
-			scope.css('height', height);
-			$.picasa.images(user, album, function (images) {
-				var picasaAlbum = '<div class="picasa-album picaslides-container" style="height: ' + height + '; width: ' + width + ';">\n';
-				$.each(images, function (i, element) {
-					picasaAlbum += '  <div class="picasa-image" style="width: ' + width + '; height: ' + height + ';' +
-					' background-repeat: no-repeat; background-image: url(' +
-						getUrlForSize(element.url, Math.max(parseInt(height, 10), parseInt(width, 10))) +
-						'); background-size: cover; background-position: center;"' +
-					'>\n';
-					picasaAlbum += '  </div>\n';
-				});
-				picasaAlbum += '</div>';
-				scope.html(picasaAlbum);
-				slideOpts.container = 'picaslides-container';
-				slideOpts.paginationClass = 'picaslide-pagination';
-				scope.slides(slideOpts);
-				if (successCallback) {
-					successCallback.apply(scope, images);
-				}
-				initControls(scope, images);
-				scope.show();
-			});
+			GALLERIES.push({element: $(this), slideOpts: slideOpts, successCallback: successCallback});
+			refresh();
 		};
 	})(jQuery);
+
+	var refresh = function () {
+		if (GALLERIES.length === 0) {
+			return;
+		}
+		var $w = $(window);
+		var wTop = $w.scrollTop();
+		var wBottom = wTop + $w.height();
+		var isInViewport = function ($element) {
+			var elTop = $element.position().top;
+			var elBottom = elTop + $element.height();
+			return (wTop < elBottom && elBottom < wBottom) || (wTop < elTop && elTop < wBottom);
+		};
+		var notRendered = [];
+		for (var i = GALLERIES.length - 1;  i >= 0; i -= 1) {
+			var gal = GALLERIES[i];
+			if (gal.element.data('rendered') !== 'yes') {
+				if (isInViewport(gal.element)) {
+					renderGallery(gal);
+					gal.element.data('rendered', 'yes');
+				} else {
+					notRendered.push(gal);
+				}
+			}
+		}
+		GALLERIES = notRendered;
+	};
+
+	var renderGallery = function (obj) {
+		var glry = obj.element;
+		var slideOpts = obj.slideOpts;
+		var successCallback = obj.successCallback;
+		var user = glry.attr('data-picaslide-username');
+		var album = glry.attr('data-picaslide-albumid');
+		if (!user || user.length < 3 || !album || album.length < 1) {
+			return;
+		}
+		var w = glry.width();
+		var width = w + 'px';
+		var height = w * 3 / 4 + 'px';
+		glry.css('width', width);
+		glry.css('height', height);
+		progressHelper.show(glry);
+		$.picasa.images(user, album, function (images) {
+			progressHelper.hide();
+			var picasaAlbum = '<div class="picasa-album picaslides-container" style="height: ' + height + '; width: ' + width + ';">\n';
+			$.each(images, function (i, element) {
+				picasaAlbum += '  <div class="picasa-image" style="width: ' + width + '; height: ' + height + ';' +
+				' background-repeat: no-repeat; background-image: url(' +
+					getUrlForSize(element.url, Math.max(parseInt(height, 10), parseInt(width, 10))) +
+					'); background-size: cover; background-position: center;"' +
+				'>\n';
+				picasaAlbum += '  </div>\n';
+			});
+			picasaAlbum += '</div>';
+			glry.html(picasaAlbum);
+			slideOpts.container = 'picaslides-container';
+			slideOpts.paginationClass = 'picaslide-pagination';
+			glry.slides(slideOpts);
+			if (successCallback) {
+				successCallback.apply(glry, images);
+			}
+			initControls(glry, images);
+			glry.show();
+		});
+	};
+
+	$(window).on('scroll', refresh);
 // close the define
 });
