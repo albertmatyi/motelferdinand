352efd37e6223f7103ef74afdf71b8fce6fb5e78
Matyas Albert Nagy <albertmatyi@gmail.com>
2013-11-18 00:28:23 +0100

    deployed v22

--- compile_js.sh
+++ compile_js.sh
@@ -2,11 +2,11 @@
 cd ./application/static/js
 rm ./main.min.js
 rm ./main.admin.min.js
-nodejs /usr/local/lib/node_modules/requirejs/bin/r.js -o ./compile_js.js
+node /usr/lib/node_modules/requirejs/bin/r.js -o ./compile_js.js
 mv ../js-build/js/main.js ./main.min.js
 rm ../js-build -R
 
-nodejs /usr/local/lib/node_modules/requirejs/bin/r.js -o ./compile_admin_js.js
+node /usr/lib/node_modules/requirejs/bin/r.js -o ./compile_admin_js.js
 mv ../js-build/js/main.admin.js ./main.admin.min.js
 rm ../js-build -R
 
