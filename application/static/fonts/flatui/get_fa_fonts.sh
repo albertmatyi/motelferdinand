ba3ab67e597d74183cd1501ffa4438ed9c61bfbe
Matyas Albert Nagy <albertmatyi@gmail.com>
2013-11-24 22:04:02 +0100

    added flatui

---
+++ application/static/fonts/flatui/get_fa_fonts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget http://netdna.bootstrapcdn.com/font-awesome/$1/fonts/fontawesome-webfont.eot?v=$1
+wget http://netdna.bootstrapcdn.com/font-awesome/$1/fonts/fontawesome-webfont.woff?v=$1
+wget http://netdna.bootstrapcdn.com/font-awesome/$1/fonts/fontawesome-webfont.ttf?v=$1
+wget http://netdna.bootstrapcdn.com/font-awesome/$1/fonts/fontawesome-webfont.svg?v=$1
